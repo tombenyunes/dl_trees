08e34f41bea9725f34cf89f1045936089ed40872
Tom Benyunes <tombenyunes@gmail.com>
2021-11-25 19:21:36 +0000

    new image served upon page refresh

--- backend/site/routes/api/api.js
+++ backend/site/routes/api/api.js
@@ -1,11 +1,35 @@
 const { exec } = require('child_process');
+const path = require('path');
+const fs = require('fs');
 
 module.exports = function (app)
 {
 
+    // app.get('/api/', function (req, res)
+    // {
+    //     var generate_image = exec('sudo sh ./scripts/generate_image.sh',
+    //         (error, stdout, stderr) => {
+    //             console.log(stdout);
+    //             console.log(stderr);
+    //             if (error !== null) {
+    //                 console.log(`exec error: ${error}`);
+    //             }
+    //         });
+    //
+    //
+    //
+    //     var dir = path.join(__dirname + '/../../../../gan/output/');
+    //     var files = fs.readdirSync(dir);
+    //
+    //     var i;
+    //     (files.length == 1) ? i = 0 : i = files.length - 1;
+    //     res.sendFile(dir + files[i]);
+    //     // res.send(file);
+    // });
+
     app.get('/api/', function (req, res)
     {
-        var generate_image = exec('sudo sh ./scripts/generate_image.sh',
+        let generate_image = exec('sudo sh ./scripts/generate_image.sh',
             (error, stdout, stderr) => {
                 console.log(stdout);
                 console.log(stderr);
@@ -13,7 +37,15 @@ module.exports = function (app)
                     console.log(`exec error: ${error}`);
                 }
             });
-        // res.sendFile(__dirname + '/tree.png');
+
+        generate_image.on('exit', function() {
+            let dir = path.join(__dirname + '/../../../../gan/output/');
+            let files = fs.readdirSync(dir);
+
+            setTimeout(() => {
+                res.sendFile(dir + files[0]);
+            }, 1);
+        })
     });
 
 }
--- backend/site/scripts/generate_image.sh
+++ backend/site/scripts/generate_image.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-bash ./../../../gan/docker_run.sh python3 ./gan/stylegan2-ada-pytorch/generate.py --outdir=./gan/output --trunc=1 --seeds=`shuf -i 1-10000 -n 1` --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
+sudo rm -d ./../../gan/output -r
+bash ./../../gan/docker_run.sh python3 ./gan/stylegan2-ada-pytorch/generate.py --outdir=./gan/output --trunc=1 --seeds=`shuf -i 1-10000 -n 1` --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
