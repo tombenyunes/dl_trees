cf47fb7855b538bc32daee4cef86fc64a2addf8b
Tom Benyunes <tombenyunes@gmail.com>
2021-11-25 19:33:52 +0000

    updated model

--- backend/site/scripts/generate_image.sh
+++ backend/site/scripts/generate_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo rm -d ./../../gan/output -r
-bash ./../../gan/docker_run.sh python3 ./gan/stylegan2-ada-pytorch/generate.py --outdir=./gan/output --trunc=1 --seeds=`shuf -i 1-10000 -n 1` --network=https://nvlabs-fi-cdn.nvidia.com/stylegan2-ada-pytorch/pretrained/metfaces.pkl
+bash ./../../gan/docker_run.sh python3 ./gan/stylegan2-ada-pytorch/generate.py --outdir=./gan/output --trunc=1 --seeds=`shuf -i 1-10000 -n 1` --network=./gan/models/network-snapshot-000120.pkl
