0dbb84f35b10c33c013c71930f36c18fe4a1350c
Tom Benyunes <tombenyunes@gmail.com>
2021-11-26 06:04:34 +0000

    platform checks

--- backend/site/routes/api/api.js
+++ backend/site/routes/api/api.js
@@ -4,32 +4,12 @@ const fs = require('fs');
 
 module.exports = function (app)
 {
-
-    // app.get('/api/', function (req, res)
-    // {
-    //     var generate_image = exec('sudo sh ./scripts/generate_image.sh',
-    //         (error, stdout, stderr) => {
-    //             console.log(stdout);
-    //             console.log(stderr);
-    //             if (error !== null) {
-    //                 console.log(`exec error: ${error}`);
-    //             }
-    //         });
-    //
-    //
-    //
-    //     var dir = path.join(__dirname + '/../../../../gan/output/');
-    //     var files = fs.readdirSync(dir);
-    //
-    //     var i;
-    //     (files.length == 1) ? i = 0 : i = files.length - 1;
-    //     res.sendFile(dir + files[i]);
-    //     // res.send(file);
-    // });
-
     app.get('/api/', function (req, res)
     {
-        let generate_image = exec('sudo sh ./scripts/generate_image.sh',
+        // if on windows
+        if (process.platform == "win32")
+        {
+            let generate_image = exec(path.join(__dirname + './../../scripts/generate_image.bat'),
             (error, stdout, stderr) => {
                 console.log(stdout);
                 console.log(stderr);
@@ -38,35 +18,37 @@ module.exports = function (app)
                 }
             });
 
-        generate_image.on('exit', function() {
-            let dir = path.join(__dirname + '/../../../../gan/output/');
-            let files = fs.readdirSync(dir);
+            generate_image.on('exit', function() {
+                let dir = path.join(__dirname + '/../../../../gan/output/');
+                let files = fs.readdirSync(dir);
+    
+                setTimeout(() => {
+                    res.sendFile(dir + files[0]);
+                }, 1);
+            })
+        }
 
-            setTimeout(() => {
-                res.sendFile(dir + files[0]);
-            }, 1);
-        })
-    });
+        // linux, mac
+        else
+        {
+            let generate_image = exec(path.join(__dirname + './../../scripts/generate_image.sh',
+                (error, stdout, stderr) => {
+                    console.log(stdout);
+                    console.log(stderr);
+                    if (error !== null) {
+                        console.log(`exec error: ${error}`);
+                    }
+                });
 
-    app.get('/apiwin/', function (req, res)
-    {
-        let generate_image = exec(path.join(__dirname + './../../scripts/generate_image.bat'),
-            (error, stdout, stderr) => {
-                console.log(stdout);
-                console.log(stderr);
-                if (error !== null) {
-                    console.log(`exec error: ${error}`);
-                }
-            });
-
-        generate_image.on('exit', function() {
-            let dir = path.join(__dirname + '/../../../../gan/output/');
-            let files = fs.readdirSync(dir);
-
-            setTimeout(() => {
-                res.sendFile(dir + files[0]);
-            }, 1);
-        })
+            generate_image.on('exit', function() {
+                let dir = path.join(__dirname + '/../../../../gan/output/');
+                let files = fs.readdirSync(dir);
+    
+                setTimeout(() => {
+                    res.sendFile(dir + files[0]);
+                }, 1);
+            })
+        }
     });
 
 }
