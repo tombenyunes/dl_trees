4de0361a67a359152a506b74a39923004f30b4e8
Tom Benyunes <tombenyunes@gmail.com>
2021-11-26 05:46:31 +0000

    updated windows support

--- backend/site/routes/api/api.js
+++ backend/site/routes/api/api.js
@@ -48,4 +48,25 @@ module.exports = function (app)
         })
     });
 
+    app.get('/apiwin/', function (req, res)
+    {
+        let generate_image = exec(path.join(__dirname + './../../scripts/generate_image.bat'),
+            (error, stdout, stderr) => {
+                console.log(stdout);
+                console.log(stderr);
+                if (error !== null) {
+                    console.log(`exec error: ${error}`);
+                }
+            });
+
+        generate_image.on('exit', function() {
+            let dir = path.join(__dirname + '/../../../../gan/output/');
+            let files = fs.readdirSync(dir);
+
+            setTimeout(() => {
+                res.sendFile(dir + files[0]);
+            }, 1);
+        })
+    });
+
 }
---
+++ backend/site/scripts/generate_image.bat
@@ -0,0 +1,3 @@
+
+@RD /S /Q "./../../gan/output"
+call ./../../gan/docker_run.bat python3 ./gan/stylegan2-ada-pytorch/generate.py --outdir=./gan/output --trunc=1 --seeds=%random% --network=./gan/models/network-snapshot-000120.pkl
