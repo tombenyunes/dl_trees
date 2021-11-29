6737dc655b3e9330c08a55c7829434b919c86152
Tom Benyunes <tombenyunes@gmail.com>
2021-11-29 12:52:52 +0000

    bug fix - check if output dir exists

--- backend/site/scripts/generate_image.bat
+++ backend/site/scripts/generate_image.bat
@@ -1,7 +1,13 @@
 
 
 :: Remove any images currently in the output directory
-@RD /S /Q "./gan/output"
+cd ./gan
+IF NOT EXIST ./output/ mkdir output
+cd ./output
+:: to avoid deleting all files in incorrect dir
+if NOT %errorlevel% == 0 (exit /b 1)
+del *.* /Q
+cd ../../
 
 :: Generate an image with a random seed to ./gan/output
 call ./gan/docker_run.bat python3 ./gan/stylegan2-ada-pytorch/generate.py --outdir=./gan/output --trunc=1 --seeds=%random% --network=./gan/models/network-snapshot-000120.pkl
