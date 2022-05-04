d6125fab2a6e3217cd58b7df7cf40664630b81f9
Tom Benyunes <tombenyunes@gmail.com>
2022-05-04 16:49:28 +0100

    added notebooks used for training

--- detectron/docker_run.sh
+++ detectron/docker_run.sh
@@ -1,8 +1,6 @@
 # To Build Image (from project root):
 # sudo docker build --build-arg USER_ID=$UID -t detectron2:v0 .
 
-echo "Docker Container Started. To exit, use the following command: 'sudo docker stop deep_learning_trees' from another terminal"
-
 docker run \
     --gpus all \
     --rm \
